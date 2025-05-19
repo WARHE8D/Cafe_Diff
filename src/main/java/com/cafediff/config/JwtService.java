aa966f421ff8babddebd5d0f7f327d01a106a6fc
WARHE8D <59991459+WARHE8D@users.noreply.github.com>
2025-05-19 16:37:49 -0600

    import jwt dependencies

--- src/main/java/com/cafediff/config/JwtService.java
+++ src/main/java/com/cafediff/config/JwtService.java
@@ -1,10 +1,30 @@
 package com.cafediff.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
-    public String extractUsername(String jwt) {
+
+    private final static String KEY = "";
+    public String extractUsername(String token) {
         return "";
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] byteKey = Decoders.BASE64.decode(KEY);
+        return Keys.hmacShaKeyFor(byteKey);
     }
 }
