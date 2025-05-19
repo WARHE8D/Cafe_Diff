102c4edf9026f37dd77fc229619ef8b07095afdb
WARHE8D <59991459+WARHE8D@users.noreply.github.com>
2025-05-19 16:25:06 -0600

    jwt filter n service

---
+++ src/main/java/com/cafediff/config/JwtAuthFilter.java
@@ -0,0 +1,33 @@
+package com.cafediff.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor//makes constructor anything that starts with private final
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt,username;
+        //auth not available
+        if(authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        username = jwtService.extractUsername(jwt);
+
+    }
+}
